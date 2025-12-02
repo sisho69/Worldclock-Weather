c3bf51cae0b8049f5f9fc897fa121a0c3caa00d6
sisho69 <95492257+sisho69@users.noreply.github.com>
2025-12-03 05:57:43 +0900

    Fix wetaher icon path

--- src/utils/weatherUtils.ts
+++ src/utils/weatherUtils.ts
@@ -1,15 +1,15 @@
-export function getWeatherIcon(wweather: string): string {
-    switch (wweather) {
+export function getWeatherIcon(weather: string): string {
+    switch (weather) {
         case "Clear":
         case "sunny":
-            return "./icons/${weather}.png";
+            return `./icons/${weather}.png`;
         case "Clouds":
         case "cloudy":
-            return "/icons/${weather}.png";
+            return `./icons/${weather}.png`;
         case "Rain":
         case "Drizzle":
-            return "/icons/${weather}.png";
+            return `./icons/${weather}.png`;
         default:
-            return "/icons/${weather}.png";
+            return `./icons/${weather}.png`;
     }
 }
